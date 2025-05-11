0fdf5d0175c735e7229715cb18cd8196ae9a0370
Alessandro <51910404+aledipa@users.noreply.github.com>
2025-05-11 17:23:24 +0200

    close #7

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -8,4 +8,37 @@ package it.unipd.mtss;
 /**
  * Class to convert integer numbers to Roman numerals 
  */
-public class IntegerToRoman {}
+public class IntegerToRoman {
+    
+    /**
+     * Converts an integer number to its Roman numeral representation
+     * 
+     * @param number Integer number to convert (1-1000)
+     * @return String representation of the Roman numeral
+     * @throws IllegalArgumentException if number is out of valid range (1-1000)
+     */
+    public static String convert(int number) {
+        if (number <= 0 || number > 1000) {
+            throw new IllegalArgumentException("Number must be between 1 and 1000");
+        }
+        
+        StringBuilder result = new StringBuilder();
+        
+        // Define Roman numeral symbols and their corresponding values
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        
+        // Iteratively find largest symbol that fits
+        int i = 0;
+        while (number > 0) {
+            if (number >= values[i]) {
+                result.append(symbols[i]);
+                number -= values[i];
+            } else {
+                i++;
+            }
+        }
+        
+        return result.toString();
+    }
+}
